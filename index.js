972e6e69edecc45212d509cb9419ddc1a0925eea
Intisar Ahmed Siyan <intisar.siyan5550@gmail.com>
2022-05-03 01:50:01 +0600

    inventory/:id single items detail route made

--- index.js
+++ index.js
@@ -27,6 +27,13 @@ async function run() {
             const inventory = await cursor.toArray();
             res.send(inventory)
         });
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            const inventory = await inventoryCollection.findOne(query);
+            res.send(inventory);
+        })
 
         // ================================ POST ============================
 
