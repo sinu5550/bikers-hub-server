3d4321966e00330726320e8940d174f396e8c093
Intisar Ahmed Siyan <intisar.siyan5550@gmail.com>
2022-05-07 05:18:03 +0600

    put method done with increase and delivered button

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-
+// ============================== uri =======================================
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eht26.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 // console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -29,17 +29,32 @@ async function run() {
         });
         app.get('/inventory/:id', async (req, res) => {
             const id = req.params.id;
-            console.log(id);
             const query = { _id: ObjectId(id) };
             const inventory = await inventoryCollection.findOne(query);
             res.send(inventory);
+            // console.log(id);
+        });
+
+        // =========================== PUT method ===================
+        app.put('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const property = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    quantity: property?.newItems?.quantity
+                }
+            };
+            result = await inventoryCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
         })
 
         // ================================ POST ============================
 
         app.post('/inventory', async (req, res) => {
             const newInventory = req.body;
-            console.log(newInventory);
+            // console.log(newInventory);
             if (!newInventory.productName || !newInventory.price || !newInventory.supplier || !newInventory.quantity || !newInventory.description || !newInventory.img) {
                 return res.send({ success: false, error: "Please provide all information" });
             }
