4c1e022a39a1da89ab145c96968829bc22d9baba
Intisar Ahmed Siyan <intisar.siyan5550@gmail.com>
2022-05-08 04:15:31 +0600

    JWT token validation done with error handling

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -10,6 +12,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req,res,next){
+    const authHeader= req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message:"unauthorized access"})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, (err,decoded)=>{
+        if(err){
+            return res.status(403).send({message:'Forbidden access'})
+
+        }
+        console.log('decoded',decoded);
+        req.decoded = decoded;
+        next();
+    })
+    
+    
+}
 
 // ============================== uri =======================================
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eht26.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -21,6 +41,16 @@ async function run() {
         await client.connect();
         const inventoryCollection = client.db('bikersHub').collection('inventory');
 
+        //================= AUTH ====================
+        app.post('/login',async(req,res)=>{
+            const user =req.body;
+            const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+                expiresIn:'1d'
+            });
+            res.send({accessToken})
+        })
+
+       // ============ Inventory API ===================================
         app.get('/inventory', async (req, res) => {
             const query = {};
             const cursor = inventoryCollection.find(query);
@@ -62,13 +92,19 @@ async function run() {
             res.send({ success: true, message: `Successfully addeded item: ${newInventory.productName}` });
         });
         // ======================== My items API =====================
-        app.get('/myItems',async(req,res)=>{
+        app.get('/myItems',verifyJWT,async(req,res)=>{
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
             // console.log(email)
-            const query={email:email};
+            if(email === decodedEmail){
+             const query={email:email};
             const cursor = inventoryCollection.find(query);
             const myItems = await cursor.toArray();
             res.send(myItems);
+            }
+            else{
+                res.status(403).send({message:'Forbidden Accsess'})
+            }
         })
         // =========================== DELETE =====================
         app.delete('/inventory/:id', async (req, res) => {
